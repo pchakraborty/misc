b2f924c0a584791be1d7ad41c1d67a5807be70c8
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2021-05-23 18:22:02 -0400

    Writing output to screen

--- gt4py-cffi/main.f90
+++ gt4py-cffi/main.f90
@@ -4,14 +4,15 @@ program main
   use Geometry, only: Geometry_T
   use Initialize, only: get_inout_fields
   use PythonFortranInterface, only: march_in_time_wrapper
-  
+
   implicit none
-  
+
   integer, parameter :: halo_size = 3
   type(Geometry_T) :: geometry
   real(real64), allocatable :: in_field(:,:,:)
   real(real64), allocatable :: out_field(:,:,:)
   integer :: dim(3)
+  integer :: i, j
 
   geometry = Geometry_T( &
        nx = 8, ny = 8, nz = 1, &
@@ -26,5 +27,6 @@ program main
        in_field, out_field, &
        dim(1), dim(2), dim(3) & ! dimensions of in/out fields
        )
-  
+  write(*,'(*(g0.2))') ((out_field(1,j,i),"  ",i=1,dim(3)), new_line("A"), j=1,dim(2))
+
 end program main
