575c24581f0048fb27fb0d16d2271dd9131077e0
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2021-06-09 13:11:32 -0400

    Streamlining environment loading

--- gt4py-cffi/setenv/discover.sh
+++ gt4py-cffi/setenv/discover.sh
@@ -1,4 +1,2 @@
-source /discover/nobackup/pchakrab/code/venv/python3/gt4py/bin/activate
-module load comp/gcc/9.3.0
-export PYTHONPATH=$PYTHONPATH:$(pwd)
+source ~pchakrab/.gtfv3.sh
 export LD_LIBRARY_PATH=$(pwd):$LD_LIBRARY_PATH
